3e412072d87995becdbf2ee72c2583e8fd2c5be0
Татьяна <ltasya72@gmail.com>
2024-09-12 09:49:00 +0300

    new file:   src/html/delivery.html   new file:   src/scss/components/_reach.scss   new file:   src/html/partners.html

--- gulpfile.js
+++ gulpfile.js
@@ -147,7 +147,7 @@ function scripts() {
     src([
       // "https://unpkg.com/aos@2.3.1/dist/aos.js",
       "node_modules/jquery/dist/jquery.js",
-      // "node_modules/swiper/swiper-bundle.js",
+      "node_modules/swiper/swiper-bundle.js",
       // "node_modules/mixitup/dist/mixitup.js",
       // "libs/mixitup3/mixitup.min.js",
       // "libs/smoothscroll/smooth-scroll.min.js",
@@ -206,7 +206,7 @@ function images() {
       .pipe(newer("images"))
       .pipe(imagemin())
 
-      .pipe(dest("images/peculiarities"))
+      .pipe(dest("images/delivery"))
   );
 }
 
--- src/js/splides.js
+++ src/js/splides.js
@@ -1,2 +1,38 @@
-const mySlider = new Splide(".splide");
-mySlider.mount();
+if (document.querySelector(".splide")) {
+  var mySlider = new Splide(".splide");
+  mySlider.mount();
+}
+
+// if (document.querySelector(".partners")) {
+//   var mySliderPartners = new Splide(".partners__slide", {
+//     // classes: {
+//     //   arrows: "splide__arrows partners__arrows",
+//     //   arrow: "splide__arrow partners__arrow",
+//     //   prev: "splide__arrow--prev partners__arrow--prev",
+//     //   next: "splide__arrow--next partners__arrow--next",
+//     // },
+//     perPage: 3,
+//     rewind: true,
+//     perMove: 1,
+//     gap: "5px",
+//   });
+//   mySliderPartners.mount();
+// }
+
+const partnersSlider = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".partners__next",
+    prevEl: ".partners__prev",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    868: {
+      slidesPerView: 3,
+    },
+  },
+});
